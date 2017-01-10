8debbc664caa25ff38aed84551493b51cee396f1
Anton <mikhaylov86@gmail.com>
2017-01-10 16:37:17 +0300

    Player change - created and test

--- tst/ru/mikhaylov/xo/model/PlayerTest.java
+++ tst/ru/mikhaylov/xo/model/PlayerTest.java
@@ -11,10 +11,30 @@ public class PlayerTest {
     @Test
     public void getName() throws Exception {
 
+        final String inputValue = "Anton";
+
+        final String expectedValue = inputValue;
+
+        final Player player = new Player(inputValue, null);
+
+        final String actualValue = player.getName();
+
+        assertEquals(expectedValue, actualValue);
+
     }
 
     @Test
     public void getFigure() throws Exception {
+
+        final Figure inputValue = Figure.X;
+
+        final Figure expectedValue = inputValue;
+
+        final Player player = new Player(null, Figure.X);
+
+        final Figure actualValue = player.getFigure();
+
+        assertEquals(expectedValue, actualValue);
 
     }
 
