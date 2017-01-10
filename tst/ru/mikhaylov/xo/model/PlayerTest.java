eb6d0901cfe4e45d6dc98feaee1ce75e13941943
Anton <mikhaylov86@gmail.com>
2017-01-10 15:53:12 +0300

    Player change - created and test

---
+++ tst/ru/mikhaylov/xo/model/PlayerTest.java
@@ -0,0 +1,21 @@
+package ru.mikhaylov.xo.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created by Anton on 10.01.2017.
+ */
+class PlayerTest {
+    @Test
+    void getName() {
+
+    }
+
+    @Test
+    void getFigure() {
+
+    }
+
+}
