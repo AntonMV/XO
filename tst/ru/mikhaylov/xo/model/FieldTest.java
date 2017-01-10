da76f564b6083b60d46ac1777cc4ee4081b76cab
AntonMV <mikhaylov86@gmail.com>
2017-01-10 20:49:25 +0300

    Field - tests for the Point added

--- tst/ru/mikhaylov/xo/model/FieldTest.java
+++ tst/ru/mikhaylov/xo/model/FieldTest.java
@@ -1,6 +1,7 @@
 package ru.mikhaylov.xo.model;
 
 import org.junit.Test;
+import ru.mikhaylov.xo.model.exceptions.AlreadyOccupiedException;
 import ru.mikhaylov.xo.model.exceptions.InvalidPointException;
 
 import java.awt.*;
@@ -24,6 +25,19 @@ public class FieldTest {
         field.setFigure(inputPoint, inputFigure);
         final Figure actualFigure = field.getFigure(inputPoint);
         assertEquals(inputFigure,actualFigure);
+    }
+
+    @Test
+    public void setFigureWhenAlreadyOccupied() throws Exception {
+        final Field field = new Field();
+        final Point inputPoint = new Point(0,0);
+        final Figure inputFigure = Figure.O;
+
+        field.setFigure(inputPoint, inputFigure);
+        try {
+            field.setFigure(inputPoint, inputFigure);
+            fail();
+        }catch (final AlreadyOccupiedException ignored){}
     }
 
     @Test
